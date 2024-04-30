c6abc597cd49b948f2877676273f5cfdea223c59
mahdiiadib <mahdiadib2001@gmail.com>
2024-04-30 07:19:23 +0600

    changed to map

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 
 struct Movie {
+    int id;
     string title, category, releaseDate;
     double budget;
     vector<string> cast;
@@ -13,12 +14,24 @@ struct Movie {
     struct cmp {
         bool operator()(const Movie &a, const Movie &b) { return a.title < b.title; }
     };
+
+    void PrintMovie(string addi="  ")
+    {
+        cout<<addi.substr(0,addi.size()-2)<<"{\n";
+        cout<<addi<<"  Id: "<<id<<",\n";
+        cout<<addi<<"  Title: "<<title<<",\n";
+        cout<<addi<<"  Category: "<<category<<",\n";
+        cout<<addi<<"  ReleaseDate: "<<releaseDate<<",\n";
+        cout<<addi<<"  Budget: "<<budget<<",\n";
+        cout<<addi<<"  Cast: "<<cast<<"\n";
+        cout<<addi.substr(0,addi.size()-2)<<"}\n\n";
+    }
 };
 
 
 struct User {
     string email;
-    set<Movie, Movie::cmp> favorites;
+    vector<int> favorites;
     
     struct cmp {
         bool operator()(const User &a, const User &b) { return a.email < b.email; }
@@ -29,7 +42,8 @@ struct User {
 class MovieLists
 {
 public:
-    set<Movie, Movie::cmp> AllMovies;
+    // vector<Movie> AllMovies;
+    map<int,Movie> AllMovies;
     
     MovieLists()
     {
@@ -48,28 +62,25 @@ public:
         for (auto& movieData : jsonData)
         {
             Movie movie;
+            movie.id=movieData["id"];
             movie.title=movieData["title"];
             movie.cast=movieData["cast"];
             movie.category=movieData["category"];
             movie.releaseDate=movieData["release_date"];
             movie.budget=movieData["budget"];
-            AllMovies.insert(movie);
+            AllMovies[movie.id]=movie;
         }
     }
 
-    static void ShowAllMovies(const set<Movie, Movie::cmp>& st, string addi="  ")
+    static void ShowAllMovies(vector<int> movieIds=vector<int>{-1}, string addi="  ")
     {
-        // sort(AllMovies.begin(), AllMovies.end(), Movie::cmp());
-        for(const Movie& m : st)
+        MovieLists ml;
+        if(!movieIds.empty() && movieIds[0]==-1)
         {
-            cout<<addi.substr(0,addi.size()-2)<<"{\n";
-            cout<<addi<<"  Title: "<<m.title<<",\n";
-            cout<<addi<<"  Category: "<<m.category<<",\n";
-            cout<<addi<<"  ReleaseDate: "<<m.releaseDate<<",\n";
-            cout<<addi<<"  Budget: "<<m.budget<<",\n";
-            cout<<addi<<"  Cast: "<<m.cast<<"\n";
-            cout<<addi.substr(0,addi.size()-2)<<"}\n\n";
+            for(auto& p : ml.AllMovies) p.second.PrintMovie(addi);
+            return;
         }
+        for(int& i : movieIds) ml.AllMovies[i].PrintMovie(addi);
     }
 };
 
@@ -77,7 +88,7 @@ public:
 class UserList
 {
 public:
-    set<User, User::cmp> AllUsers;
+    map<string, vector<int>> AllUsers;
     
     UserList()
     {
@@ -96,39 +107,72 @@ public:
         {
             User user;
             user.email=userData["email"];
-            for (auto& f : userData["favorites"])
-            {
-                Movie movie;
-                movie.title = f["title"];
-                movie.cast = f["cast"];
-                movie.category = f["category"];
-                movie.releaseDate = f["release_date"];
-                movie.budget = f["budget"];
-                user.favorites.insert(movie);
-            }
-            AllUsers.insert(user);
+            for (auto& f : userData["favorites"]) user.favorites.push_back(f);
+            AllUsers[user.email]=user.favorites;
         }
     }
 
     void ShowAllUsers()
     {
-        for(const User& u : AllUsers)
+        for(auto& p : AllUsers)
         {
             cout<<"{\n";
-            cout<<"  email: "<<u.email<<",\n";
+            cout<<"  email: "<<p.first<<",\n";
             cout<<"  Favorites: "<<"\n";
-            MovieLists::ShowAllMovies(u.favorites, "    ");
+            // cout<<p.second<<'\n';
+            MovieLists::ShowAllMovies(p.second, "    ");
             cout<<"}\n\n";
         }
+    }
+
+    void addUser(string email)
+    {
+        User newUser;
+        newUser.email = email;
+        AllUsers[email]=vector<int>{};
+    }
+
+    void addFavoriteMovie(string email, int movieId)
+    {
+        if(AllUsers.find(email)!=AllUsers.end())
+        {
+            AllUsers[email].push_back(movieId);
+            cout<<"Movie ID: "<<movieId<<" added to favorites\n";
+            return;
+        }
+        cout<<"User not found: "<<email<<'\n';
+    }
+
+    void removeFavoriteMovie(string email, int movieId)
+    {
+        if(AllUsers.find(email)!=AllUsers.end())
+        {
+            for(auto it=AllUsers[email].begin(); it!=AllUsers[email].end(); it++)
+            {
+                if(*it==movieId)
+                {
+                    AllUsers[email].erase(it);
+                    cout<<"Movie ID: "<<movieId<<" removed from favorites\n";
+                    return;
+                }
+            }
+            cout<<"Movie ID: "<<movieId<<" not found in favorites\n";
+            return;
+        }
+        cout<<"User not found: "<<email<<'\n';
     }
 };
 
 
 int main()
 {
-    MovieLists ml;
-    // ml.ShowAllMovies(ml.AllMovies);
+    // MovieLists ml;
+    // ml.ShowAllMovies();
+
+    // UserList ul;
+    // ul.ShowAllUsers();
 
     UserList ul;
+    ul.addFavoriteMovie("user3@example.com", 15);
     ul.ShowAllUsers();
 }
